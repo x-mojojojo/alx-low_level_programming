389433e5e3488a80c0562eaddefa8f6b1ab01be6
Jackson Kibara <jackkibbz@gmail.com>
2022-09-09 02:34:52 +0300

    printf

---
+++ 0x00-hello_world/5-print.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - using the print function
+ * Return: self explained
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
