55d805b3ff6ca50e94959a8d531bf99042e69cb5
Jackson Kibara <jackkibbz@gmail.com>
2022-09-09 21:31:53 +0300

    update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,11 @@
  */
 int main(void)
 {
-	int n;
-
+    int n;
+    
     for (n = 122; n >= 97; n--)
-    {
         putchar(n);
-    }
-	putchar('\n');
-	return (0);
+    
+    putchar('\n');
+    return (0);
 }
