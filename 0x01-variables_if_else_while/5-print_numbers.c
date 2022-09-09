0b4bb19f57fbbf7ca3f78a7c71a61383340896c0
Jackson Kibara <jackkibbz@gmail.com>
2022-09-09 15:54:58 +0300

    new file:   5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	return (0);
+}
