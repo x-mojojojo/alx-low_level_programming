99a889305b122468ddbb24f0da4cce3cabda54fc
Jackson Kibara <jackkibbz@gmail.com>
2022-09-09 14:33:55 +0300

    modified:   0x01-variables_if_else_while/3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,18 +7,18 @@
 int main(void)
 {
 	char alphabet = 'a';
-    char ALPHABET = 'A';
+	char ALPHABET = 'A';
 	char new_line = '\n';
 
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		putchar(alphabet);
-	}
-		
+	}	
     for (ALPHABET = 'A'; ALPHABET <= 'Z'; ALPHABET++)
 	{
 		putchar(ALPHABET);
 	}
 	putchar(new_line);
+	
 	return (0);
 }
