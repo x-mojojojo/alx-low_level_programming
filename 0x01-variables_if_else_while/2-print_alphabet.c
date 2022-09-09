6c707bfd61befa0c52758edd4e82e4da2550e1d8
Jackson Kibara <jackkibbz@gmail.com>
2022-09-09 14:10:51 +0300

    new file:   0x01-variables_if_else_while/2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++) {
+		putchar(alphabet);
+	}
+}
