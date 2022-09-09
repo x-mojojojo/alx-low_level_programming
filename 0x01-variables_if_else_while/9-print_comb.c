4e3ec9daba715f2e1d98a1cead1f0e66e8e35c6a
Jackson Kibara <jackkibbz@gmail.com>
2022-09-09 22:20:51 +0300

    update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,15 +4,16 @@
  * main - numbers separated by a comma
  * Return: return 0 if successful
  */
-int main(void)
+int main()
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
-	{
+	for (i = 48; i <= 57; i++) {
 		putchar(i);
-		putchar(',');
-		putchar(' ');
+		if (i < 57) {
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
