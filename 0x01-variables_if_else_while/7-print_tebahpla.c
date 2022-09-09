9b63f064c6f8da9bd4cad7a9469fb62eea7353f7
Jackson Kibara <jackkibbz@gmail.com>
2022-09-09 21:23:52 +0300

    created 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ * Return: return 0 if successful
+ */
+int main(void)
+{
+	int n;
+	for (n = 122; n >= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
