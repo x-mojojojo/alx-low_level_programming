2acbcd7b8d5a70279416d01cdd9cca4dcbac371c
Jackson Kibara <jackkibbz@gmail.com>
2022-09-13 01:34:51 +0300

    update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,13 +16,13 @@ int main(void)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
-				putchar('\t');
+				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
-					putchar('\t');
+					putchar(' ');
 				}
 			}
 
