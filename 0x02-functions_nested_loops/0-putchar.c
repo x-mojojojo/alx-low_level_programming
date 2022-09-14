c6b16f5003299988de43be2cde6e772530476777
Jackson Kibara <jackkibbz@gmail.com>
2022-09-14 23:02:26 +0300

    update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,21 @@
 #include "main.h"
 
 /**
-  *
-  *main - print _putchar 
-  * 
-  *return - return value if successful
-  */
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-    int i;
-
-    for (i = 0; i <= 8; i++)
-        putchar(_PUTCHAR[i]);
-    putchar('\n');
-    return (0);
+  int i;
+  
+  for (i = 0; i <= 8; i++)
+  {
+    putchar(_PUTCHAR[i]);
+  }
+  putchar('\n');
+  return (0);
 }
