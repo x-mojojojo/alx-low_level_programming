75b5e20267444fdd97c2cbc1d4c507d668faf0c0
Jackson Kibara <jackkibbz@gmail.com>
2022-09-16 13:33:56 +0300

    update main.h

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
+ */
+void more_numbers(void)
+{
+	int i, j, round;
+	
+	for (round = 1; round <= 10; round++)
+	{
+		for (i = 48; i <= 49; i++)
+		{
+			for (j = 48; j <= 57; j++)
+			{
+				if (i == 49 && j == 53)
+					break;
+				else if (i==48)
+				{
+					putchar(j);
+				}
+				else
+				{
+					_putchar(i);
+					_putchar(j);
+				}
+			}
+		}
+		_putchar('\n');
+	}
+	
+}
