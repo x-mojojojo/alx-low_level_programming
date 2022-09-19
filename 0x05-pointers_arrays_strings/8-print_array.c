118c99966e051d130b7efcd671f697253a1ba8fd
Jackson Kibara <jackkibbz@gmail.com>
2022-09-20 01:46:40 +0300

    update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,14 +8,14 @@
  * Return: no return.
  */
 
-void print_array(int* a, int n)
+void print_array(int *a, int n)
 {
 	int i = 0;
-	
+
 	for (; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		
+
 		if (i != n - 1)
 			printf(", ");
 	}
