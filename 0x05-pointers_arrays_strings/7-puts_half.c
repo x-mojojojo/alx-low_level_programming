2d9a6847a47fe89e623125b16074cecb42efc72a
Jackson Kibara <jackkibbz@gmail.com>
2022-09-20 01:44:38 +0300

    update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,13 +8,21 @@
 
 void puts_half(char *str)
 {
-	int i;
+	int count = 0, i;
 
-	for (i = 0; i <= 9 ; i++)
+	while (count >= 0)
 	{
-		if (i > 9 / 2)
-		{
-			_putchar(str[i]);
-		}
+		if (str[count] == '\0')
+			break;
+		count++;
 	}
+
+	if (count % 2 == 1)
+		i = count / 2;
+	else
+		i = (count - 1) / 2;
+
+	for (i++; i < count; i++)
+		_putchar(str[i]);
+	_putchar('\n');
 }
