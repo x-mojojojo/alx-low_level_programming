b54455ff165cde3a998b221a9d934e8179c0becc
Jackson Kibara <jackkibbz@gmail.com>
2022-09-20 01:14:30 +0300

    update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,11 +10,13 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	do
+	if (str[0] != '\0')
 	{
-		_putchar(str[i]);
-		i++;
-
-	} while (str[i] != '\0');
+		do
+		{
+			_putchar(str[i]);
+			i++;
+		} while (str[i] != '\0');
+	}
 	_putchar('\n');
 }
