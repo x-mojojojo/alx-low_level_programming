8f4e9937ab006c629015b5ea82274f00317bfa20
Jackson Kibara <jackkibbz@gmail.com>
2022-09-22 13:08:59 +0300

    update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,47 +10,28 @@ char *cap_string(char *str)
 {
     int i;
 
-    for (i = 0; str[i]; i++)
-    {
-        if (str[i] == '}')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '{')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == ')')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '(')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '"')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '?')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '!')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '.')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == ';')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == ',')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == ' ')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '\n')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-        if (str[i] == '\t')
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-                str[i + 1] -= 32;
-    }
+    if (str[0] != '\0')
+        for (i = 0; str[i]; i++)
+        {
+            if (str[i] == '}' ||
+                str[i] == '{' ||
+                str[i] == ')' ||
+                str[i] == '(' ||
+                str[i] == '"' ||
+                str[i] == '?' ||
+                str[i] == '!' ||
+                str[i] == '.' ||
+                str[i] == ';' ||
+                str[i] == ',' ||
+                str[i] == ' ' ||
+                str[i] == '\n' ||
+                str[i] == '\t')
+            {
+                if (str[i + 1] >= 97 && str[i + 1] <= 122)
+                {
+                    str[i + 1] = str[i+1] - 32;
+                }
+            }    
+        }
     return (str);
 }
