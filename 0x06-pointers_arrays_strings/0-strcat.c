e349dcdc93b01d5dd420f6746c12bb4a8af68c1d
Jackson Kibara <jackkibbz@gmail.com>
2022-09-23 14:04:10 +0300

    update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,11 +6,12 @@
  * @src: source.
  * Return: the pointer to dest.
  */
-char *_strcat(char *dest, char *src) {
-    int i, j;
-
-    for (i = 0; dest[i] != '\0'; i++);
-    for (j = 0; src[j] != '\0'; j++, i++)
-        dest[i] = src[j];
-    return (dest);
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+	
+	for (i = 0; dest[i] != '\0'; i++);
+	for (j = 0; src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	return (dest);
 }
