24fb0da6ebf87e49847599e93905855de30f95da
Jackson Kibara <jackkibbz@gmail.com>
2022-09-27 05:42:21 +0300

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -14,9 +14,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i <= n; i++)
-	{
-		s[i] = b;
-	}
+	if (n != 0)
+		for (i = 0; i <= n; i++)
+		{
+			s[i] = b;
+		}
 	return (s);
 }
