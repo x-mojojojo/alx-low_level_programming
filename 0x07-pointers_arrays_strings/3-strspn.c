9b81215f5a02c602e4cf7330f755c59814a9e334
Jackson Kibara <jackkibbz@gmail.com>
2022-09-27 16:59:13 +0300

    added 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* _strspn - Gets the length of a prefix substring.
+* @s: String where substring will look.
+* @accept: Substring of accepted chars.
+* Return: Length of occurrence.
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int sum = 0;
+	int i, j;
+	int true = 0;
+
+	for (i = 0;accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (accept[i] == s[j])
+			{
+				true = 1;
+				break;
+			}
+		}
+		if (true = 1)
+			sum++;
+		else
+			break;
+	}
+	return (sum);
+}
