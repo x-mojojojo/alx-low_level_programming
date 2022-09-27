7acc77d22ae3405e33b33b4eb2bc76f3d6cb5c7e
Jackson Kibara <jackkibbz@gmail.com>
2022-09-27 20:47:11 +0300

    updated 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,8 +5,9 @@
  * @haystack: The string to be searched.
  * @needle: The substring to be located.
  *
- * Return: If the substring is located - a pointer to the beginning
- *                                       of the located substring.
+ * Return: If the substring is located
+ *          a pointer to the beginning
+ *         of the located substring.
  * If the substring is not located - NULL.
  */
 
