fe0a106b2c171084bf4e9006e1fae7ed4212020b
Jackson Kibara <jackkibbz@gmail.com>
2022-09-30 20:57:54 +0300

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ * main - print number of arguments passed
+ * @argc: argument count
+ * @args: arguments
+ * Return: 0 if success
+ */
+
+int main(int argc, char* args[])
+{
+	printf("%d\n", argc -1);
+	return (0);
+}
