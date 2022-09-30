51efe48fd032278d26b605245566abb7b9c290d7
Jackson Kibara <jackkibbz@gmail.com>
2022-10-01 01:57:01 +0300

    added 2-ars.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - list all arguments.
+ * @argc: argument count.
+ * @argv: argument passed.
+ * Return: 0 - success.
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
