cf41e18756167eaa962787cf54074d1df8b989aa
xmojojojo <jackkibbz@gmail.com>
2022-09-30 18:22:59 +0300

    updated 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,9 @@
 
 /**
  * main - main function
- * @argc - argument count
+ * @argc: argument count
  * @argv: argument vector
- * return: 0
+ * Return: 0 if success
  */
 
 int main(int argc, char *argv[])
