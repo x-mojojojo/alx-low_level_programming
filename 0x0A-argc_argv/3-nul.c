778edea48023f076ebae65e71a285efe2f6cb19d
Jackson Kibara <jackkibbz@gmail.com>
2022-10-01 14:07:33 +0300

    Multiply 2 integer passed by arguments

---
+++ 0x0A-argc_argv/3-nul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - multiply 2 integers passed by argumnt.
+ * @argc: argument count.
+ * @argv: argument passed.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 3)
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	else
+		printf("Error\n");
+	return (0);
+}
