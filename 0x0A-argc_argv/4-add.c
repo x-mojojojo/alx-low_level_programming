6772966bf6f3ac8daa7a2702aaf1e2d8963ab975
Jackson Kibara <jackkibbz@gmail.com>
2022-10-02 23:38:18 +0300

    added 5-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - add values passed to the command line.
+ * @argc: argument count.
+ * @argv: argument passed.
+ * Return: 0 - success.
+ */
+
+int main(int argc, char* argv[])
+{
+	int i, j, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
