5a76ef9e606525b000030c554eab47f5178b2c83
Jackson Kibara <jackkibbz@gmail.com>
2022-10-05 10:02:06 +0300

    line 17 ||size == 0;

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	char *ch = (char*)malloc(size *sizeof(char));
 	unsigned int i;
 
-  if (ch == NULL)
+  if (ch == NULL || size == 0)
 	{
 		return (NULL);
 	}
