238768c5757fdef15ca6db848b2247fc5db4dcb4
Jackson Kibara <jackkibbz@gmail.com>
2022-10-05 09:59:42 +0300

    *sizeof

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 
 char *create_array(unsigned int size, char c)
 {
-	char *ch = (char*)malloc(size * sizeof(char));
+	char *ch = (char*)malloc(size *sizeof(char));
 	unsigned int i;
 
   if (ch == NULL)
