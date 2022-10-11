b067b3ff67436770eb4d7d26b2b54be65dff9bdf
Jackson Kibara <jackkibbz@gmail.com>
2022-10-12 00:38:07 +0300

    alx??

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,10 +14,12 @@ void print_dog(struct dog* d)
 			printf("Name: %s\n", (*d).name);
 		else
 			printf("Name: (nil)\n");
+
 		if ((*d).age > 0)
 			printf("Age: %f\n", (*d).age);
 		else
 			printf("Age: (nil)\n");
+
 		if ((*d).owner != NULL)
 			printf("Owner: %s\n", (*d).owner);
 		else
