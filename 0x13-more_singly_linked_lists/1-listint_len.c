0de27684bf01485be88368046e0d44ed030baf30
Jackson Kibara <jackkibbz@gmail.com>
2022-10-26 23:02:00 +0300

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * listint_len - prints number of elements in a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of elements in the list.
+ */
+
+size_t listint_len(const listint_t* h)
+{
+    size_t elements;
+
+    for (elements = 0; h != NULL; elements++, h = h->next);
+
+    return (elements);
+}
