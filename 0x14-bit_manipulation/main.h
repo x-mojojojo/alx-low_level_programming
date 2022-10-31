813cb9d1b415b9ea6b5d89af66b0a2e952ce6a96
Jackson Kibara <jackkibbz@gmail.com>
2022-11-01 01:49:35 +0300

    update main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 size_t _strlen(const char *str);
-
+void print_binary(unsigned long int n);
+void print_rev(char *s);
 
 #endif /*MAIN_H*/
