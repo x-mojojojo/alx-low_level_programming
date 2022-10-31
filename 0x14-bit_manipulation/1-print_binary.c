9cb80fabce10a2372be59ef98a23d34e30a300fe
Jackson Kibara <jackkibbz@gmail.com>
2022-11-01 01:48:34 +0300

    decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string
+ * Return: NULL
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = _strlen(s);
+
+	if (s[0] != '\0')
+	{
+		do {
+			i--;
+			if (s[i] == '1' || s[i] == '0')
+				_putchar(s[i]);
+		} while (i > 0);
+	}
+	else if (i == 0)
+		_putchar('0');
+}
+
+/**
+ * print_binary - convert decimal to binary
+ * @n: decimal to be converted
+ * Return: NULL
+ */
+void print_binary(unsigned long int n)
+{
+	size_t binary, decimal, i = 0;
+	char c[20] = {0};
+
+	while (n >= 0)
+	{
+		if (n == 0)
+		{
+			break;
+		}
+		else
+		{
+			decimal = n >> 1;
+			binary = n - (decimal << 1);
+			c[i] = (binary + '0');
+		}
+		n = n >> 1;
+		i++;
+	}
+	print_rev(c);
+}
