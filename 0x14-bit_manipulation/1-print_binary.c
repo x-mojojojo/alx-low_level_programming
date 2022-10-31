8af0088a2459b7622468a547f8956b727ee3da7d
Jackson Kibara <jackkibbz@gmail.com>
2022-11-01 02:33:20 +0300

    code redundancy

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,61 +2,13 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
-=======
- * _strlen - returns length of string
- * @str: string
- * Return: length of string
- */
-size_t _strlen(const char *str)
-{
-	const char *eos = str;
-
-	while (*eos++)
-		;
-
-	return (eos - str - 1);
-}
-
-/**
- * print_rev - prints string in reverse
- * @s: string
- * Return: NULL
- */
-void print_rev(char *s)
-{
-	int i;
-
-	i = _strlen(s);
-
-	if (s[0] != '\0')
-	{
-		do {
-			i--;
-			if (s[i] == '1' || s[i] == '0')
-				_putchar(s[i]);
-		} while (i > 0);
-	}
-	else if (i == 0)
-		_putchar('0');
-}
-
-/**
->>>>>>> 003de3c236caed26d7455dd69f861d80ed2e8f0c
  * print_binary - convert decimal to binary
  * @n: decimal to be converted
  * Return: NULL
  */
 void print_binary(unsigned long int n)
 {
-<<<<<<< HEAD
 	if (n > 1)
-=======
-	size_t binary, decimal, i = 0;
-	char c[20] = {0};
-
-	while (n > 0)
->>>>>>> 003de3c236caed26d7455dd69f861d80ed2e8f0c
 	{
 		print_binary(n >> 1);
 	}
